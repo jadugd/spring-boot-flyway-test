d283506a2f5a69e971a461f025ce4c75e9893ee8
jadugd <jdestian@gmail.com>
2018-11-26 10:13:02 +0700

    db migration/V1__init.sql

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE posts (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  title varchar(100) NOT NULL,
+  description varchar(250) NOT NULL,
+  content varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_title (title)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
