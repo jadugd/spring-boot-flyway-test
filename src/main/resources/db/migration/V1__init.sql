f07c47bf235e6580acee7e6f5c2a77712c22eeb7
jadugd <jdestian@gmail.com>
2018-11-26 10:58:08 +0700

    i change the prop

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,7 +2,7 @@ CREATE TABLE posts (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   title varchar(100) NOT NULL,
   description varchar(250) NOT NULL,
-  content varchar(50) DEFAULT NULL,
+  content text DEFAULT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY UK_title (title)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
